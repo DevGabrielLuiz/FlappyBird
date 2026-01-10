625bd0a3d8f22dc53d841839d33cfe201eaf0022
Gabriel <gabrieluizdev@gmail.com>
2026-01-10 08:46:55 -0300

    Transformando o código em site

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,6 +1,14 @@
 import pygame
 import os
 import random 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
 
 TELA_LARGURA = 500
 TELA_ALTURA = 800
@@ -231,4 +239,6 @@ def main():
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
+# deubg=True -> atualizar dinâmicamente o site 
     main()
